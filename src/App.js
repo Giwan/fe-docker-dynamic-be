8253ed95d5eeefd090ada8b3f6c99553069538d8
Giwan Persaud <giwan@knowble.nl>
2018-09-10 23:26:07 +0200

    minor cleanup in app.js

--- src/App.js
+++ src/App.js
@@ -4,29 +4,17 @@ import "./App.css";
 
 class App extends Component {
   componentDidMount() {
-    // fetch backend endpoint
     fetch("/backendapiroute")
       .then(rsp => rsp.json())
       .then(rsp => {
         window.apiHost = rsp.apiHost;
       })
-      .catch(e =>
-        console.error(
-          "something went wrong while trying to get the api host",
-          e
-        )
-      );
+      .catch(e => console.error("failed to get the api host", e));
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Sample app testing dynamic backend</h1>
       </div>
     );
   }
